d522874dec4cb1031022f7bfc1b77bf2d73bd41e
Geek-Tekina <tpbc1717@gmail.com>
2022-02-22 14:11:23 +0530

    In search of an easy problem

---
+++ Imp_10.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int n ;
+ cin>>n;
+ for(int i =0 ; i<n;++i)
+ {
+     bool ch ;
+     cin>>ch;
+     if(ch==1)
+     {
+         cout<<"HARD";
+         return 0 ;
+     }
+ }
+ cout<<"EASY";
+ 
+return 0;
+}
