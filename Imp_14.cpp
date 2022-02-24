699cab1e97d6b844912aaa90594941989cfc94f0
Geek-Tekina <tpbc1717@gmail.com>
2022-02-24 08:52:17 +0530

    Wrong Subtraction

---
+++ Imp_14.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int n , t;
+ cin>>n>>t;
+ while(t--)
+ {
+     if(n%10==0)
+     n=n/10;
+     else
+     n-=1;
+
+ }
+ cout<<n;
+return 0;
+}
